ccd9f80b73c7eb6d24e1d4e6953832496b4dbc43
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2021-02-02 11:34:20 +0530

    cards only repeat when >52

--- js/helpers.js
+++ js/helpers.js
@@ -1,4 +1,26 @@
+const lessThan52 = numberOfCards => {
+	let arr = {};
+	let i = 0;
+
+	while (i < numberOfCards) {
+		const rand = Math.floor(Math.random() * allImages.length);
+
+		if (rand in arr) {
+			continue;
+		} else {
+			arr[rand] = true;
+			i++;
+		}
+	}
+
+	return Object.keys(arr);
+};
+
 const getRandomlyShuffledCards = numberOfCards => {
+	if (numberOfCards <= 52) {
+		return lessThan52(numberOfCards);
+	}
+
 	let arr = [];
 	let i = 0;
 
