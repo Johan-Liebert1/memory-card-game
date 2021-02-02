cf8b01345864701e8420820c8e41fd9200eda0d8
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2021-02-02 11:40:28 +0530

    cards in random order

--- js/helpers.js
+++ js/helpers.js
@@ -1,23 +1,24 @@
 const lessThan52 = numberOfCards => {
-	let arr = {};
+	let arr = [];
 	let i = 0;
 
 	while (i < numberOfCards) {
 		const rand = Math.floor(Math.random() * allImages.length);
 
-		if (rand in arr) {
+		if (arr.includes(rand)) {
 			continue;
 		} else {
-			arr[rand] = true;
+			arr.push(rand);
 			i++;
 		}
 	}
-
-	return Object.keys(arr);
+	console.log(arr);
+	return arr;
 };
 
 const getRandomlyShuffledCards = numberOfCards => {
 	if (numberOfCards <= 52) {
+		console.log("numberOfCards <= 52");
 		return lessThan52(numberOfCards);
 	}
 
